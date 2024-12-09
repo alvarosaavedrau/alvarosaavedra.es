a88091fbb83ee5cae7f4f8d7ac263a0c080907bd
alvarosaavedrau <cloud@alvarosaavedra.es>
2024-12-09 19:53:28 +0100

    chore(theme): update logo and project link

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,7 @@
 export default {
-    logo: <span>My Nextra Documentation</span>,
+    logo: <span>alvarosaavedra.es</span>,
     project: {
-        link: 'https://github.com/shuding/nextra'
+        link: 'https://github.com/alvarosaavedra/alvarosaavedra.es'
     }
     // ... other theme options
 }
