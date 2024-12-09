58851ea8bc7c8617b615449917a2dcf3037402a8
alvarosaavedrau <cloud@alvarosaavedra.es>
2024-12-09 19:54:18 +0100

    fix: github username updated

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,7 @@
 export default {
     logo: <span>alvarosaavedra.es</span>,
     project: {
-        link: 'https://github.com/alvarosaavedra/alvarosaavedra.es'
+        link: 'https://github.com/alvarosaavedrau/alvarosaavedra.es'
     }
     // ... other theme options
 }
