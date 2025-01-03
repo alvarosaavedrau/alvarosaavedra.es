00072275b335a8bded3b8f92b2b7b063f5ff7ddc
alvarosaavedrau <cloud@alvarosaavedra.es>
2025-01-03 12:50:58 +0100

    chore: remove sitebehaviour analysis

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,24 +1,3 @@
-import Head from 'next/head';
-
 export default function App({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                <script type="text/javascript" dangerouslySetInnerHTML={{
-                    __html: `
-                    (function() {
-                      var sbSiteSecret = "3b809295-8bbc-4521-9fc4-c93ef735668e"
-                      window.sitebehaviourTrackingSecret = sbSiteSecret;
-                      var scriptElement = document.createElement('script');
-                      scriptElement.async = true;
-                      scriptElement.id = 'site-behaviour-script-v2';
-                      scriptElement.src = 'https://sitebehaviour-cdn.fra1.cdn.digitaloceanspaces.com/index.min.js?sitebehaviour-secret=' + sbSiteSecret;
-                      document.head.appendChild(scriptElement); 
-                    })()
-                    `
-                }} />
-            </Head>
-            <Component {...pageProps} />
-        </>
-    );
+    return <Component {...pageProps} />
 }
