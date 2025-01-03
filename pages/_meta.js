45cf8f1a2a49db1664853698a00cf68a8479e49d
alvarosaavedrau <cloud@alvarosaavedra.es>
2025-01-03 19:21:16 +0100

    chore: add new buttons to navbar

--- pages/_meta.js
+++ pages/_meta.js
@@ -2,4 +2,16 @@ export default {
     index: 'Página principal',
     docs: 'Documentación',
     links: 'Enlaces',
+    publicIp: {
+        title: 'Cuál es mi IP',
+        type: 'page',
+        href: 'https://ip.alvarosaavedra.es',
+        newWindow: true
+    },
+    CV: {
+        title: 'Currículum',
+        type: 'page',
+        href: 'https://alvarosaavedra.cloud',
+        newWindow: true
+    }
 }
