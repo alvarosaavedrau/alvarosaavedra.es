8c5951fbee9b5b404ecf5b782c11b4891092a87e
alvarosaavedrau <cloud@alvarosaavedra.es>
2025-01-03 22:38:05 +0100

    add new cloudflare workers blog page

--- pages/_meta.js
+++ pages/_meta.js
@@ -1,16 +1,43 @@
 export default {
-    index: 'Página principal',
-    docs: 'Documentación',
-    links: 'Enlaces',
+    '*': {
+        type: 'page',
+    },
+    docs: {
+        title: 'Documentación',
+        type: 'menu',
+        items: {
+            terraform: {
+                title: 'Terraform',
+                href: '/docs/terraform',
+            },
+            kubernetes: {
+                title: 'Kubernetes',
+                href: '/docs/kubernetes'
+            }
+        }
+    },
+    blog: {
+        title: 'Blog',
+        type: 'menu',
+        items: {
+            'Cloudflare Workers': {
+                title: 'Cloudflare Workers',
+                href: '/blog/cloudflareworkers',
+            }
+        }
+    },
+    links: {
+        title: 'Enlaces útiles',
+        href: '/links',
+        newWindow: false
+    },
     publicIp: {
         title: 'Cuál es mi IP',
-        type: 'page',
         href: 'https://ip.alvarosaavedra.es',
         newWindow: true
     },
     CV: {
         title: 'Currículum',
-        type: 'page',
         href: 'https://alvarosaavedra.cloud',
         newWindow: true
     }
