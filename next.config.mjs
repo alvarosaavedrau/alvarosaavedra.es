34f60cf82eb6acb4d79177b1de05f2a987b00cfc
alvarosaavedrau <cloud@alvarosaavedra.es>
2025-02-07 23:40:05 +0100

    add domain allowed to use images

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
-import nextra from 'nextra'
+import nextra from 'nextra';
 
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
-  defaultShowCopyCode: true
-})
+  defaultShowCopyCode: true,
+});
 
-export default withNextra()
-
-// If you have other Next.js configurations, you can pass them as the parameter:
-// export default withNextra({ /* other next.js config */ })
+export default withNextra({
+  images: {
+    domains: ['cdn.alvarosaavedra.es'],
+  },
+});
