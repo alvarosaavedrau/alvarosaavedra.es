960cdeda12ed7b85b48b9a6cc296de68fb0b9b75
alvarosaavedrau <cloud@alvarosaavedra.es>
2026-05-28 22:12:40 +0200

    fix: remove unused Vercel analytics components from App

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,7 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
-
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <SpeedInsights />
-      <Analytics />
     </>
   )
 }
